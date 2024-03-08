8fb7269e00ed905bff6aa22c3cf39e4f10565b58
nom utilisateur de votre repo git <morzak78@gmail.com>
2024-03-08 16:42:29 +0100

    ♻️ F02-NavBar: Add NavbarRightSide

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { styled } from "styled-components";
 import { theme } from "../theme";
-import { ShoppingCartIcon } from "@heroicons/react/24/outline";
 import Search from "./Search";
 import NavbarLeftSide from "./NavbarLeftSide";
+import NavbarRightSide from "./NavbarRightSide";
 
 export default function NavBar() {
   return (
@@ -13,20 +13,7 @@ export default function NavBar() {
         <div className="search">
           <Search />
         </div>
-        <div className="right">
-          <div className="right-l-text">
-            <div className="right-l-text-1">Hello, Sign In</div>
-            <div className="right-l-text-2">Accounts & List</div>
-          </div>
-          <div className="right-r-text">
-            <div className="right-r-text-1">Returns</div>
-            <div className="right-r-text-2">& Orders</div>
-          </div>
-          <div className="icon">
-            <ShoppingCartIcon className="img" />
-            <div>Cart</div>
-          </div>
-        </div>
+        <NavbarRightSide />
       </div>
       <div className="nav-bottom">
         <span>Todays Deals</span>
@@ -62,51 +49,8 @@ const NavBarStyled = styled.header`
         width: 38%;
       }
     }
-    .right {
-      margin-right: 15px;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-
-      .right-l-text {
-        padding-left: 15px;
-        padding-right: 15px;
-
-        .right-l-text-1 {
-          font-size: 14px;
-        }
-        .right-l-text-2 {
-          font-size: 16px;
-          font-weight: bold;
-        }
-      }
-      .right-r-text {
-        padding-left: 15px;
-        padding-right: 15px;
-
-        .right-r-text-1 {
-          font-size: 14px;
-        }
-        .right-r-text-2 {
-          font-size: 16px;
-          font-weight: bold;
-        }
-      }
-      .icon {
-        display: flex;
-        align-items: end;
-
-        .img {
-          height: 48px;
-          width: 48px;
-        }
-        span {
-          font-size: 16px;
-          font-weight: bold;
-        }
-      }
-    }
   }
+
   .nav-bottom {
     background: ${theme.colors.light_blue};
     height: 35px;
