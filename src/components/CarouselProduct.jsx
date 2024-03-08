e2d5b4e573f4c86c59ac3fd9489936d4692ecd25
nom utilisateur de votre repo git <morzak78@gmail.com>
2024-03-08 20:10:37 +0100

    🚧 F03-ProductPage: Add ProductPage and his routing

--- src/components/CarouselProduct.jsx
+++ src/components/CarouselProduct.jsx
@@ -1,43 +1,45 @@
-import React from 'react'
-import {Swiper, SwiperSlide} from 'swiper/react';
-import { Navigation, Autoplay } from 'swiper/modules';
-import { styled } from 'styled-components';
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Autoplay } from "swiper/modules";
+import { styled } from "styled-components";
+import { Link } from "react-router-dom";
 
 export default function CarouselProduct() {
   return (
     <CarouselProductStyled>
-        <div className="title">Best Sellers</div>
-        <Swiper
-            slidesPerView={5}
-            loop={true}
-            spaceBetween={10}
-            navigation={true}
-            modules={[Navigation]}
-        >
-                {Array.from({length: 9}, (_, i) => <SwiperSlide key={i}>
-                    <img src={`/images/product_${i}_small.jpg`} />
-                </SwiperSlide>
-                )}
-
-        </Swiper>
+      <div className="title">Best Sellers</div>
+      <Swiper
+        slidesPerView={5}
+        loop={true}
+        spaceBetween={10}
+        navigation={true}
+        modules={[Navigation]}
+      >
+        {Array.from({ length: 9 }, (_, i) => (
+          <SwiperSlide key={i}>
+            <Link to={`/product`}>
+              <img src={`/images/product_${i}_small.jpg`} />
+            </Link>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </CarouselProductStyled>
-  )
+  );
 }
 
 const CarouselProductStyled = styled.div`
-background: white;
-padding: 10px;
-margin-top: 15px;
+  background: white;
+  padding: 10px;
+  margin-top: 15px;
 
-.title{
+  .title {
     font-size: 21px;
     font-weight: bold;
     padding: 5px;
     margin-bottom: 8px;
-}
+  }
 
-img{
+  img {
     padding: 0 60px;
-}
-
+  }
 `;
