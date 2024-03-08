fc67ed2aeba0fce94edc7b68ea3b2fb93500b305
nom utilisateur de votre repo git <morzak78@gmail.com>
2024-03-08 12:48:59 +0100

    🚧 F02-Navbar: Add Search and his style

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,118 +1,127 @@
-import React from 'react'
-import { styled } from 'styled-components';
-import { theme } from '../theme';
-import {ShoppingCartIcon} from '@heroicons/react/24/outline'
+import React from "react";
+import { styled } from "styled-components";
+import { theme } from "../theme";
+import { ShoppingCartIcon } from "@heroicons/react/24/outline";
+import Search from "./Search";
 
 export default function NavBar() {
   return (
     <NavBarStyled>
-        <div className="container">
-            <div className='left'>
-                <img src="/images/amazon.png" alt="logo amazon" />
-                <div className='left-text'>
-                    <div className='left-text-1'>Deliver to</div>
-                    <div className='left-text-2'>United Kingdom</div>
-                </div>
-            </div>
-            <div>middle</div>
-            <div className='right'>
-                <div className='right-l-text'>
-                    <div className='right-l-text-1'>Hello, Sign In</div>
-                    <div className='right-l-text-2'>Accounts & List</div>
-                </div>
-                <div className='right-r-text'>
-                    <div className='right-r-text-1'>Returns</div>
-                    <div className='right-r-text-2'>& Orders</div>
-                </div>
-                <div className="icon">
-                    <ShoppingCartIcon className='img'/>
-                    <div>Cart</div>
-                </div>
-            </div>
+      <div className="container">
+        <div className="left">
+          <img src="/images/amazon.png" alt="logo amazon" />
+          <div className="left-text">
+            <div className="left-text-1">Deliver to</div>
+            <div className="left-text-2">United Kingdom</div>
+          </div>
         </div>
+        <div className="search">
+          <Search />
+        </div>
+        <div className="right">
+          <div className="right-l-text">
+            <div className="right-l-text-1">Hello, Sign In</div>
+            <div className="right-l-text-2">Accounts & List</div>
+          </div>
+          <div className="right-r-text">
+            <div className="right-r-text-1">Returns</div>
+            <div className="right-r-text-2">& Orders</div>
+          </div>
+          <div className="icon">
+            <ShoppingCartIcon className="img" />
+            <div>Cart</div>
+          </div>
+        </div>
+      </div>
     </NavBarStyled>
-  )
+  );
 }
 
 const NavBarStyled = styled.header`
-min-width: 1000px;
-background: ${theme.colors.default};
-color: white;
-height: 60px;
+  min-width: 1000px;
+  background: ${theme.colors.default};
+  color: white;
+  height: 60px;
 
-.container{
+  .container {
     height: 100%;
     display: flex;
     justify-content: space-between;
 
-    .left{
-        margin-left: 15px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
+    .left {
+      margin-left: 15px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
 
-        img{
-            height: 35px;
-            margin: 2px;
+      img {
+        height: 35px;
+        margin: 2px;
+      }
+
+      .left-text {
+        padding-left: 15px;
+        padding-right: 15px;
+
+        .left-text-1 {
+          font-size: 14px;
         }
-
-        .left-text{
-            padding-left: 15px;
-            padding-right: 15px;
-
-            .left-text-1{
-                font-size: 14px;
-            }
-            .left-text-2{
-                font-size: 16px;
-                font-weight: bold;
-            }
+        .left-text-2 {
+          font-size: 16px;
+          font-weight: bold;
         }
-    }
-    .right{
-        margin-right: 15px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-
-        .right-l-text{
-            padding-left: 15px;
-            padding-right: 15px;
-
-            .right-l-text-1{
-                font-size: 14px;
-            }
-            .right-l-text-2{
-                font-size: 16px;
-                font-weight: bold;
-            }
-        }
-        .right-r-text{
-            padding-left: 15px;
-            padding-right: 15px;
-
-            .right-r-text-1{
-                font-size: 14px;
-            }
-            .right-r-text-2{
-                font-size: 16px;
-                font-weight: bold;
-            }
-        }
-        .icon{
-            display: flex;
-            align-items: end;
-
-            .img{
-                height: 48px;
-            width: 48px;
-            }
-            span{
-                font-size: 16px;
-                font-weight: bold;
-            }
-        }
+      }
     }
 
-}
+    .search {
+      width: 60%;
+      display: flex;
+      justify-content: left;
+      align-items: center;
+    }
+    .right {
+      margin-right: 15px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      .right-l-text {
+        padding-left: 15px;
+        padding-right: 15px;
+
+        .right-l-text-1 {
+          font-size: 14px;
+        }
+        .right-l-text-2 {
+          font-size: 16px;
+          font-weight: bold;
+        }
+      }
+      .right-r-text {
+        padding-left: 15px;
+        padding-right: 15px;
+
+        .right-r-text-1 {
+          font-size: 14px;
+        }
+        .right-r-text-2 {
+          font-size: 16px;
+          font-weight: bold;
+        }
+      }
+      .icon {
+        display: flex;
+        align-items: end;
+
+        .img {
+          height: 48px;
+          width: 48px;
+        }
+        span {
+          font-size: 16px;
+          font-weight: bold;
+        }
+      }
+    }
+  }
 `;
---
+++ src/components/Search.jsx
@@ -0,0 +1,65 @@
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import React from "react";
+import { styled } from "styled-components";
+import { theme } from "../theme";
+
+export default function Search() {
+  return (
+    <SearchStyled>
+      <select className="select">
+        <option>All Category</option>
+        <option>Deals</option>
+        <option>Amazon</option>
+        <option>Fashion</option>
+        <option>Home</option>
+      </select>
+      <input type="text" className="input" />
+      <div className="button-container">
+        <button className="button">
+          <MagnifyingGlassIcon />
+        </button>
+      </div>
+    </SearchStyled>
+  );
+}
+
+const SearchStyled = styled.div`
+  background: ${theme.colors.yellow};
+  width: 100%;
+  height: 65%;
+  border-radius: 4px;
+  display: flex;
+  justify-content: left;
+  align-items: center;
+
+  .select {
+    height: 100%;
+    width: 20%;
+    background: rgb(230, 230, 230);
+    border: 1px solid rgb(205, 205, 205);
+    border-radius: 4px;
+    padding-left: 8px;
+  }
+
+  .input {
+    height: 100%;
+    flex: 1;
+    border: none;
+  }
+
+  .button-container {
+    width: 48px;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    .button {
+      background: transparent;
+      border: none;
+      color: rgb(51, 51, 51);
+      height: 27px;
+      width: 27px;
+    }
+  }
+`;
