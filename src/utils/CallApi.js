921e22bebf308d567262ab4faf142cc1d7c4df78
nom utilisateur de votre repo git <morzak78@gmail.com>
2024-03-10 11:03:38 +0100

    🚧 F03-ProductPage: made the called API function

--- src/utils/CallApi.js
+++ src/utils/CallApi.js
@@ -1,3 +1,14 @@
 import axios from "axios";
+import { BASE_URL } from "./constants";
 
-export const callApi = () => {};
+const config = {
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+};
+
+export const callApi = async (ressource) => {
+  const { data } = await axios.get(`${BASE_URL}/${ressource}`, config);
+  return data;
+};
