aea435bc5695cea60929d01031c772e9d6385d19
nom utilisateur de votre repo git <morzak78@gmail.com>
2024-03-10 18:05:43 +0100

    🚧 F03-ProductPage: Add grid layout to ProductPage

--- src/components/ProductPage.jsx
+++ src/components/ProductPage.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { callApi } from "../utils/CallApi";
+import { styled } from "styled-components";
+import { theme } from "../theme";
 
 export default function ProductPage() {
   const { id } = useParams();
@@ -18,5 +20,61 @@ export default function ProductPage() {
 
   if (!product?.title) return <h1>Loading Product...</h1>;
 
-  return product && <div>ProductPage {product.title}</div>;
+  return (
+    product && (
+      <ProductPageStyled>
+        <div className="container">
+          <div className="grid-container">
+            <div className="left">
+              <img src={`${product.image}`} alt="" />
+            </div>
+            <div className="middle"></div>
+            <div className="right"></div>
+          </div>
+        </div>
+      </ProductPageStyled>
+    )
+  );
 }
+
+const ProductPageStyled = styled.div`
+  height: 100vh;
+  background: ${theme.colors.background};
+
+  .container {
+    min-width: 1000px;
+    max-width: 1500px;
+    background: orange;
+    margin: 0 auto;
+
+    .grid-container {
+      display: grid;
+      grid-template-columns: repeat(
+        10,
+        minmax(0, 1fr)
+      ); /* Crée une grille avec 10 colonnes de largeur égale */
+      gap: 0.5rem;
+
+      .left {
+        grid-column: 1 / span 3;
+        background: white;
+        padding: 10px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        img {
+          width: 60%;
+        }
+      }
+      .middle {
+        grid-column: 4 / span 5;
+        background: pink;
+      }
+      .right {
+        grid-column: 9 / span 2;
+        background: green;
+      }
+    }
+  }
+`;
