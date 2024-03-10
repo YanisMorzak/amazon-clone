4b5572bc3d30bd9a78bac3804a72347d33359a36
nom utilisateur de votre repo git <morzak78@gmail.com>
2024-03-10 11:48:47 +0100

    🚧 F03-ProductPage: API call in ProductPage

--- src/components/ProductPage.jsx
+++ src/components/ProductPage.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
+import { callApi } from "../utils/CallApi";
 
 export default function ProductPage() {
   const { id } = useParams();
-  return <div>ProductPage {id}</div>;
+  const [product, setproduct] = useState(null);
+
+  const getProduct = () => {
+    callApi(`data/products.json`).then((productResults) => {
+      setproduct(productResults[id]);
+    });
+  };
+
+  useEffect(() => {
+    getProduct();
+  }, []);
+
+  if (!product?.title) return <h1>Loading Product...</h1>;
+
+  return product && <div>ProductPage {product.title}</div>;
 }
