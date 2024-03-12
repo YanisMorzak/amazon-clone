6b0bc80abd72bc1f6a34ff3fcd1c37d0cdbba248
nom utilisateur de votre repo git <morzak78@gmail.com>
2024-03-12 11:55:39 +0100

    🚧   F04-Search: Add logic to retrieve data sent by Search

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -1,5 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import { callApi } from "../utils/CallApi";
 
 export default function SearchResults() {
+  const [searchParams] = useSearchParams();
+  const [products, setProducts] = useState(null);
+
+  const getSearchResults = () => {
+    const searchTerm = searchParams.get("searchTerm");
+    const category = searchParams.get("category");
+
+    callApi(`data/search.json`).then((searchResults) => {
+      const categoryResults = searchResults[category];
+      if (searchTerm) {
+        const results = categoryResults.filter((product) =>
+          product.title.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+        setProducts(results);
+      } else {
+        setProducts(categoryResults);
+      }
+    });
+  };
+
+  useEffect(() => {
+    getSearchResults();
+  }, [searchParams]);
+
   return <div>SearchResults</div>;
 }
