ad9ce0d91b0e3c6c7e36ce0cc83987ba7c057bf0
nom utilisateur de votre repo git <morzak78@gmail.com>
2024-03-13 10:01:59 +0100

    🚧   F05-Checkout: retrieve the state value from cartSlice and display it next to the basket icon

--- src/components/Navbar/NavbarRightSide.jsx
+++ src/components/Navbar/NavbarRightSide.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { styled } from "styled-components";
 import { ShoppingCartIcon } from "@heroicons/react/24/outline";
+import { useSelector } from "react-redux";
 
 export default function NavbarRightSide() {
+  const cart = useSelector((state) => state.cart.productsNumber);
   return (
     <NavbarRightSideStyled>
       <div className="right-l-text">
@@ -15,6 +17,9 @@ export default function NavbarRightSide() {
       </div>
       <div className="icon">
         <ShoppingCartIcon className="img" />
+        <div className="number-container">
+          <div className="number">{cart}</div>
+        </div>
         <div>Cart</div>
       </div>
     </NavbarRightSideStyled>
@@ -52,6 +57,7 @@ const NavbarRightSideStyled = styled.div`
     }
   }
   .icon {
+    position: relative;
     display: flex;
     align-items: end;
 
@@ -59,9 +65,15 @@ const NavbarRightSideStyled = styled.div`
       height: 48px;
       width: 48px;
     }
-    span {
-      font-size: 16px;
-      font-weight: bold;
+
+    .number-container {
+      .number {
+        position: absolute;
+        top: 25%;
+        left: 30%;
+        font-weight: bold;
+        color: orange;
+      }
     }
   }
 `;
