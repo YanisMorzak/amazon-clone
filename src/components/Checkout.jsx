1af4bbeeec92b59cb9a510d5fd21c2096227804a
nom utilisateur de votre repo git <morzak78@gmail.com>
2024-03-15 10:24:57 +0100

    ♻️  F06-Refacto: Add CheckoutSummarySide

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,38 +1,16 @@
 import React from "react";
 import { styled } from "styled-components";
 import { theme } from "../theme";
-import { useSelector } from "react-redux";
-import { US_CURRENCY } from "../utils/constants";
 import CheckoutProductSide from "./CheckoutProductSide";
+import CheckoutSummarySide from "./CheckoutSummarySide";
 
 export default function Checkout() {
-  const itemsNumber = useSelector((state) => state.cart.productsNumber);
-  const subtotal = useSelector((state) =>
-    state.cart.products.reduce(
-      (subtotal, product) => subtotal + product.price * product.quantity,
-      0
-    )
-  );
-
   return (
     <CheckoutStyled>
       <div className="container">
         <div className="grid-container">
           <CheckoutProductSide />
-          <div className="checkout-side">
-            <div className="delivery-checkout">
-              Your order qualifies for{" "}
-              <span className="delivery-bold">FREE DELIVERY.</span> Delivery
-              Details
-            </div>
-            <div>
-              Subtotal ({itemsNumber} items):{" "}
-              <span className="sub-price-checkout">
-                {US_CURRENCY.format(subtotal)}
-              </span>
-            </div>
-            <button className="btn-checkout">Proceed to Checkout</button>
-          </div>
+          <CheckoutSummarySide />
         </div>
       </div>
     </CheckoutStyled>
@@ -52,40 +30,6 @@ const CheckoutStyled = styled.div`
       display: grid;
       grid-template-columns: repeat(8, minmax(0, 1fr));
       gap: 10px;
-
-      .checkout-side {
-        grid-column: 7 / span 2;
-        height: 250px;
-        background: white;
-        padding: 20px 15px;
-        border-radius: 3px;
-
-        .delivery-checkout {
-          color: green;
-          margin-bottom: 15px;
-          .delivery-bold {
-            font-weight: bold;
-          }
-        }
-
-        .sub-price-checkout {
-          font-size: 20px;
-          font-weight: bold;
-        }
-
-        .btn-checkout {
-          cursor: pointer;
-          background: #ffd814;
-          border: none;
-          border-radius: 3px;
-          margin-top: 20px;
-          padding: 10px;
-          width: 80%;
-          &:hover {
-            background: #eeca13;
-          }
-        }
-      }
     }
   }
 `;
